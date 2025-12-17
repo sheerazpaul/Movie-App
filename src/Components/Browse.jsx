6b28deb4e6cd730d88decbedc924f40a4eb9f51c
sheerazpaul <sheerazpaulp@gmail.com>
2025-12-17 14:00:23 +0500

    just change

--- src/Components/Browse.jsx
+++ src/Components/Browse.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Input, Button } from "@heroui/react";
-import List from "./Dropdown";
+import List from "../Components/Dropdown";
 import {
   orderByData,
   qualityData,
